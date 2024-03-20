9ed5a713c6c53ad62dd6c69398ab0d09b6542734
Patryk Miszke <131891673+Pamisz@users.noreply.github.com>
2024-03-21 00:09:46 +0100

    Update server.java

--- Lab3/server.java
+++ Lab3/server.java
@@ -1,39 +1,32 @@
 package org.example;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.net.*;
 
 public class server {
-	private static final Logger LOGGER = Logger.getLogger(server.class.getName());
-	private final int port;
+	private ServerSocket server = null;
 
 	public server(int port) {
-		this.port = port;
-	}
-
-	public void start() {
-		try (ServerSocket serverSocket = new ServerSocket(port)) {
-			LOGGER.info("Serwer uruchomiony na porcie " + port + ". Oczekiwanie na klientów...");
+		try {
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+			System.out.println("Waiting for a clients ...");
 
 			while (true) {
-				Socket clientSocket = serverSocket.accept();
-				LOGGER.info("Nowe połączenie z klientem: " + clientSocket.getInetAddress());
+				Socket clientSocket = server.accept();
+				System.out.println("New client connected: " + clientSocket.getInetAddress());
 
 				ClientHandler clientHandler = new ClientHandler(clientSocket);
 				Thread clientThread = new Thread(clientHandler);
 				clientThread.start();
 			}
 		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, "Błąd serwera: " + e.getMessage(), e);
+			throw new RuntimeException(e);
 		}
 	}
 
 	public static void main(String[] args) {
 		int port = 8080;
 		server serv = new server(port);
-		serv.start();
 	}
 }
